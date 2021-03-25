823c2e0702e0396b3cc06daa5b00678b5c918db4
Sashi <sashi@Sashis-MacBook-Pro.local>
2021-03-25 20:58:59 +0530

    adding dotenv to read all variable from .env file

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const apiRoutes = require('./routes');
+const envParams = require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const apiRoutes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,8 +13,8 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // API routes
-app.use('/api', apiRoutes)
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Test app listening on port ${PORT}`)
+  console.log(`Test app listening on port ${PORT}`);
 });
