e8e954ccc908e3b2607f3fd99a033f7247a8b8b4
Sashi <sashi@Sashis-MacBook-Pro.local>
2021-03-25 20:55:48 +0530

    adding config file for github apis constants

---
+++ api/config.js
@@ -0,0 +1,9 @@
+const config = {
+    GITHUB_API : "https://api.github.com",
+    GITHUB_ACCESS_TOKEN_API : "https://github.com/login/oauth/access_token",
+    CLIENT_ID : process.env.CLIENT_ID,
+    CLIENT_SECRET : process.env.CLIENT_SECRET
+}
+
+
+module.exports =  config
